db27d7cb9c5d641ec49a1fd6d9a417d139e11c47
Nico <nico.riedel@bihealth.de>
2021-09-14 10:22:09 +0200

    Update & simplify tests

--- barzooka/tests/test_barzooka.py
+++ barzooka/tests/test_barzooka.py
@@ -1,15 +1,14 @@
 import pytest
+import os
 from .. import barzooka
 
+b = barzooka.Barzooka()
+    
 import pathlib
-pathlib.PosixPath = pathlib.WindowsPath
-
-def test_barzooka_class_loads():
-    b = barzooka.Barzooka("barzooka/barzooka.pkl")
-    assert True
+if os.name == 'nt':
+    pathlib.PosixPath = pathlib.WindowsPath
 
 def test_barzooka_screen_img():
-    b = barzooka.Barzooka("barzooka/barzooka.pkl")
     assert b.predict_from_img("barzooka/examples/img/text1.jpg") == [['text']]
     assert b.predict_from_img("barzooka/examples/img/violin1.jpg") == [['violin']]
     assert b.predict_from_img("barzooka/examples/img/box1.jpg") == [['box']]
@@ -17,13 +16,15 @@ def test_barzooka_screen_img():
     assert b.predict_from_img("barzooka/examples/img/dot1.jpg") == [['dot']]
 
 def test_barzooka_screen_img_folder():
-    b = barzooka.Barzooka("barzooka/barzooka.pkl")
     screening_results = b.predict_from_img_folder("barzooka/examples/img")
-    assert screening_results[1] == [['box'], ['box', 'dot'], ['box', 'dot'], ['dot'], ['dot'], ['text'], ['violin']]
+    #make sure that screening results are given in right order
+    #should this code be included in the predict function?s
+    screening_results_sorted = [category for file,category in 
+                                sorted(zip(screening_results[0],screening_results[1]))]
+
+    assert screening_results_sorted == [['box'], ['box', 'dot'], ['box', 'dot'], ['dot'], ['dot'], ['text'], ['violin']]
 
 def test_barzooka_screen_pdf():
-    b = barzooka.Barzooka("barzooka/barzooka.pkl")
-
     screening_results = b.predict_from_file("barzooka/examples/pdf/text.pdf")
     assert screening_results == {'approp': 0,
                                  'bar': 0,
@@ -55,7 +56,11 @@ def test_barzooka_screen_pdf():
                                  'paper_id': 'doc'}
 
     screening_results = b.predict_from_file("barzooka/examples/pdf/doc.pdf", pagewise = True)
-    assert screening_results == [['box'],
+    #make sure that screening results are given in right order
+    #should this code be included in the predict function?s
+    screening_results_sorted = [category for file,category in 
+                                sorted(zip(screening_results[0],screening_results[1]))]
+    assert screening_results_sorted == [['box'],
                                  ['box', 'dot'],
                                  ['box', 'dot'],
                                  ['dot'],
