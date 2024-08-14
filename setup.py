e7d848e8d70a713fb371dfb485e8af98e0e285ab
Vladislav Nachev <vladislav.nachev@protonmail.com>
2024-08-14 15:10:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,17 @@ from setuptools import setup
 
 setup(
   name='Barzooka',
-  version='0.1.0',
-  author='Nico Riedel',
-  author_email='nico.riedel@bih-charite.de',
+  version='0.1.1',
+  author='Nico Riedel, Vladislav Nachev',
+  author_email='vladislav.nachev@charite.de',
   packages=['barzooka'],
   license='LICENSE.txt',
   description='Barzooka is a deep convolutional neural network that screens publication PDFs and checks for bar graphs of continuous data and more informative alternatives to bar graphs, like dot plots, box plots and histograms.',
   long_description=open('README.md').read(),
   install_requires=[
-      "fastai >= 2.0",
-      "numpy",
-      "pandas",
-      "pytest",
-      "importlib_resources",
+      "fastai==2.7.13",
+      "pandas==2.2.2",
+      "pytest==8.3.2",
+      "importlib_resources==6.4.0",
   ],
 )
